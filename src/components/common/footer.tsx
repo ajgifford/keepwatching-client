116189c01d925938591d3d96657807c16873f3a8
Andy Gifford <ajgifford@users.noreply.github.com>
2025-01-29 10:39:46 -0600

    Additional footer updates

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -13,12 +13,21 @@ export function Footer() {
       <Typography variant="caption" color="textPrimary" gutterBottom>
         Gifford Family Dev
       </Typography>
-      <br />
-      <TMDBIcon />
-      <br />
-      <Typography variant="caption" color="textPrimary" gutterBottom>
-        This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB
-      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '8px',
+        }}
+      >
+        <TMDBIcon />
+        <Typography variant="caption" color="textPrimary">
+          This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by{' '}
+          <a href="https://www.themoviedb.org/">TMDB</a> | Additional information is provided by Streaming Availability
+          API by <a href="https://www.movieofthenight.com/about/api">Movie of the Night</a>
+        </Typography>
+      </Box>
     </Box>
   );
 }
