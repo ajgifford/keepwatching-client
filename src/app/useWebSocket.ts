df6c1e9047f5430daff37da017c85389d36c105f
Andy Gifford <ajgifford@users.noreply.github.com>
2025-02-09 09:17:10 -0600

    Update websocket to only connect when logged in and to use a token

--- src/app/useWebSocket.ts
+++ src/app/useWebSocket.ts
@@ -4,22 +4,38 @@ import { BACKEND_WEBSOCKET_URL } from './constants/constants';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { selectCurrentAccount } from './slices/accountSlice';
 import { setActiveProfile } from './slices/activeProfileSlice';
-import { io } from 'socket.io-client';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { Socket, io } from 'socket.io-client';
+
+let socket: Socket | null = null;
 
 export const useWebSocket = () => {
   const dispatch = useAppDispatch();
   const account = useAppSelector(selectCurrentAccount);
+  const auth = getAuth();
 
   useEffect(() => {
-    const socket = io(BACKEND_WEBSOCKET_URL);
-    socket.on('globalUpdate', () => {
-      if (account) {
-        dispatch(setActiveProfile({ accountId: account.id, profileId: account.default_profile_id }));
+    if (!account) return;
+
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const token = await user.getIdToken();
+
+        socket = io(BACKEND_WEBSOCKET_URL, {
+          auth: { token },
+        });
+
+        socket.on('globalUpdate', () => {
+          dispatch(setActiveProfile({ accountId: account.id, profileId: account.default_profile_id }));
+        });
       }
     });
 
     return () => {
-      socket.disconnect();
+      if (socket) {
+        socket.disconnect();
+        socket = null;
+      }
     };
-  }, [dispatch, account]);
+  }, [dispatch, account, auth]);
 };
