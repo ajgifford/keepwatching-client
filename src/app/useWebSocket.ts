9a1d98a0586d81d11a759ab1a5d0de400968626a
Andy Gifford <ajgifford@users.noreply.github.com>
2025-04-04 08:14:48 -0500

    Update web socket connection logic

--- src/app/useWebSocket.ts
+++ src/app/useWebSocket.ts
@@ -1,51 +1,70 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { BACKEND_WEBSOCKET_URL } from './constants/constants';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { selectCurrentAccount } from './slices/accountSlice';
 import { reloadActiveProfile, reloadProfileEpisodes, updateAfterAddShowFavorite } from './slices/activeProfileSlice';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { User, getAuth, onAuthStateChanged } from 'firebase/auth';
 import { Socket, io } from 'socket.io-client';
-
-let socket: Socket | null = null;
 
 export const useWebSocket = () => {
   const dispatch = useAppDispatch();
   const account = useAppSelector(selectCurrentAccount);
   const auth = getAuth();
 
+  const socketRef = useRef<Socket | null>(null);
+
   useEffect(() => {
     if (!account) return;
 
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
+    let unsubscribe: () => void;
+
+    const setupSocket = async (user: User) => {
+      if (!socketRef.current || !socketRef.current.connected) {
+        if (socketRef.current) {
+          socketRef.current.disconnect();
+        }
+
         const token = await user.getIdToken();
 
-        socket = io(BACKEND_WEBSOCKET_URL, {
+        socketRef.current = io(BACKEND_WEBSOCKET_URL, {
           auth: { token: token, account_id: account.id },
-          reconnectionAttempts: 5, // Limits to 5 reconnection attempts
-          reconnectionDelay: 2000, // Wait 2 seconds before retrying
+          reconnectionAttempts: 5,
+          reconnectionDelay: 2000,
         });
 
-        socket.on('showsUpdate', () => {
+        socketRef.current.on('showsUpdate', () => {
           dispatch(reloadActiveProfile());
         });
 
-        socket.on('moviesUpdate', () => {
+        socketRef.current.on('moviesUpdate', () => {
           dispatch(reloadActiveProfile());
         });
 
-        socket.on('updateShowFavorite', async (data) => {
+        socketRef.current.on('updateShowFavorite', async (data) => {
           await dispatch(updateAfterAddShowFavorite(data.show));
           await dispatch(reloadProfileEpisodes());
         });
       }
+    };
+
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setupSocket(user).catch(console.error);
+      } else if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
     });
 
     return () => {
-      if (socket) {
-        socket.disconnect();
-        socket = null;
+      if (unsubscribe) {
+        unsubscribe();
+      }
+
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
       }
     };
   }, [dispatch, account, auth]);
